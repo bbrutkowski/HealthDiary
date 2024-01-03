d12b48f090bfe54405f7f391fc8493c03c82dfde
blazejrutkowski <blazejrutkowskiwork.gmail.com>
2024-01-03 17:22:44 +0100

    Logi/Register mediator handle

--- HealthDiary/HealthDiary.API/Controllers/UserController.cs
+++ HealthDiary/HealthDiary.API/Controllers/UserController.cs
@@ -2,6 +2,9 @@
 using HealthDiary.API.Context.Model;
 using HealthDiary.API.Context.Model.Dto;
 using HealthDiary.API.Helpers;
+using HealthDiary.API.MediatR.Commands;
+using HealthDiary.API.MediatR.Handlers;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,50 +14,25 @@ namespace HealthDiary.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly DataContext _context;
-        private readonly string UserNotFoundError = "User not found";
-        private readonly string UserCredentialsError = "User name or password are wrong";
+        private readonly IMediator _mediator;
 
-        public UserController(DataContext dataContext)
-        {
-            _context = dataContext;
-        }
+        public UserController(IMediator mediator) => _mediator = mediator;
 
         [HttpPost(nameof(Login))]
-        public async Task<IActionResult> Login([FromBody] UserDto userDto, CancellationToken token)
+        public async Task<IActionResult> Login([FromBody] LoginUserRequest request, CancellationToken token)
         {
-            if (userDto is null) return BadRequest();
-
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.IsActive && x.Name == userDto.UserName, token);
-
-            if (user is null) return NotFound(UserNotFoundError);
-
-            if (!PasswordHasher.Verify(userDto.Password, user.Password)) return NotFound(UserCredentialsError);
-
-            var role = user.Role;
-
-            return Ok(new { Role = role });
+            var result = await _mediator.Send(request, token);
+            if (result.IsFailure) return BadRequest(result);
+            return Ok(result);
         }
 
         [HttpPost]
         [Route(nameof(Register))]
-        public async Task<IActionResult> Register([FromBody] User user, CancellationToken token)
+        public async Task<IActionResult> Register([FromBody] RegisterUserRequest request, CancellationToken token)
         {
-            if (user is null) return BadRequest();
-
-            user.Password = PasswordHasher.Hash(user.Password);
-
-            try
-            {
-                await _context.Users.AddAsync(user, token);
-
-                await _context.SaveChangesAsync();
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
+            var result = await _mediator.Send(request, token);
+            if (result.IsFailure) return BadRequest(result);
+            return Ok(result);
         }
     }
 }
