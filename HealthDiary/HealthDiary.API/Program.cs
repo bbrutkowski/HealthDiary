ff88b74f16adcd106631390f482fa133aceefa19
blazejrutkowski <blazejrutkowskiwork.gmail.com>
2024-01-03 10:32:41 +0100

    MediatR

--- HealthDiary/HealthDiary.API/Program.cs
+++ HealthDiary/HealthDiary.API/Program.cs
@@ -1,4 +1,5 @@
 using HealthDiary.API.Context;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(typeof(Program));
 
 builder.Services.AddCors(option =>
 {
