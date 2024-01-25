0b46f9aca41e626aab0ae8f332e529b9a5185c20
blazejrutkowski <blazejrutkowskiwork.gmail.com>
2024-01-25 18:03:37 +0100

    sleep table

---
+++ HealthDiary/HealthDiary.API/Context/Model/Main/Sleep.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthDiary.API.Context.Model.Main
+{
+    public class Sleep
+    {
+        [Key]
+        public int Id { get; set; }
+        public decimal SleepTime { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ HealthDiary/HealthDiary.API/Migrations/20240125170135_SleepTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthDiary.API.Migrations
+{
+    public partial class SleepTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sleeps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SleepTime = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sleeps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sleeps_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sleeps_UserId",
+                table: "Sleeps",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sleeps");
+        }
+    }
+}
