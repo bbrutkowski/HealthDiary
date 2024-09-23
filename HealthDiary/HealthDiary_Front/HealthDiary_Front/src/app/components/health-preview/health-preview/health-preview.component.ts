2f3995e19d3834cfaa1c32a0ef798d4cca19eaf1
blazejrutkowski <blazejrutkowskiwork.gmail.com>
2024-09-23 14:57:44 +0200

    preview component

---
+++ HealthDiary/HealthDiary_Front/HealthDiary_Front/src/app/components/health-preview/health-preview/health-preview.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { SleepInfoDto } from 'src/app/models/sleep-info-dto';
+import { TotalActivityDto } from 'src/app/models/total-activity';
+import { WeeklyNutritionDto } from 'src/app/models/weekly-nutrition-dto';
+import { WeightDto } from 'src/app/models/weight-dto';
+
+@Component({
+  selector: 'app-health-preview',
+  templateUrl: './health-preview.component.html',
+  styleUrl: './health-preview.component.css'
+})
+export class HealthPreviewComponent {
+  @Input() weights: Array<WeightDto> = [];
+  @Input() monthlyActivities: TotalActivityDto;
+  @Input() weeklyNutritionDto: WeeklyNutritionDto;
+  @Input() lastSleepInfo: SleepInfoDto;
+
+}
